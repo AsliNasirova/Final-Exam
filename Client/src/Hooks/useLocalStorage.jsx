cd51af709a8213d593744779aee828d6a65d11ec
Aysun hes <tu6ye9fl8@code.edu.az>
2024-01-25 17:44:41 +0400

    added

--- Client/src/Hooks/useLocalStorage.jsx
+++ Client/src/Hooks/useLocalStorage.jsx
@@ -1,9 +1,16 @@
 import React from 'react'
+import { useEffect } from 'react';
 import { useState } from 'react'
 import { useParams } from 'react-router-dom'
 
-const useLocalStorage = () => {
-    const[local,setLocal]=useState([])
+const useLocalStorage = (key) => {
+    const[local,setLocal]=useState(useLocalStorage.getItem(key)? JSON.parse(localStorage.getItem(key)):[]);
+
+    useEffect(()=>{
+        localStorage.setItem(key,JSON.stringify(local))
+    },[local])
+        
+    
     const handleLocal=useParams('')
 
   return [local,handleLocal]
